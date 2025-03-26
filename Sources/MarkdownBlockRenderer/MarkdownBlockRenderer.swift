4f4a4a8ff6ac7de2796f62b514e7a4cc9a73223b
Christian Tietze <me@christiantietze.de>
2025-03-26 09:30:12 +0100

    process target blocks into hashed filenames

--- Sources/MarkdownBlockRenderer/MarkdownBlockRenderer.swift
+++ Sources/MarkdownBlockRenderer/MarkdownBlockRenderer.swift
@@ -12,10 +12,39 @@ where Target: Markdown.BlockMarkup {
 	public func renderedFiles(
 		document: Markdown.Document
 	) async throws -> [URL] {
-		return []
+		var collectedBlocks: [Target] = []
+		var visitor = TargetVisitor { target in
+			collectedBlocks.append(target)
+		}
+		visitor.visit(document)
+
+		var renderedBlocks: [(Target, URL)] = []
+		for collectedBlock in collectedBlocks {
+			let result = try await render(block: collectedBlock)
+			renderedBlocks.append((collectedBlock, result))
+		}
+
+		return renderedBlocks.map(\.1)
 	}
 
 	func render(block: Target) async throws -> URL {
-		fatalError()
+		// TODO: Compute hash string
+		let hash = "rendered-" + String(block.format().hashValue)
+		let url = outputDirectory.appending(path: hash)
+		// TODO: render into file at URL
+		return url
+	}
+}
+
+struct TargetVisitor<Target>: Markdown.MarkupWalker
+where Target: Markdown.BlockMarkup {
+	let visit: (Target) -> ()
+
+	mutating func defaultVisit(_ markup: any Markup) -> () {
+		if let target = markup as? Target {
+			visit(target)
+		} else {
+			descendInto(markup)
+		}
 	}
 }
