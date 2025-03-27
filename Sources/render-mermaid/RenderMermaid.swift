ad273365dff7d508e05054afa98ecd2cbde47d40
Christian Tietze <me@christiantietze.de>
2025-03-27 09:03:37 +0100

    add -o/--outdir option

--- Sources/render-mermaid/RenderMermaid.swift
+++ Sources/render-mermaid/RenderMermaid.swift
@@ -1,7 +1,7 @@
 import ArgumentParser
-import MarkdownBlockRenderer
 import Foundation
 import Markdown
+import MarkdownBlockRenderer
 
 extension Pipe {
 	static func stdin(string: String) throws -> Pipe {
@@ -10,6 +10,10 @@ extension Pipe {
 		try stdin.fileHandleForWriting.close()
 		return stdin
 	}
+}
+
+struct RenderError: Error {
+	let message: String
 }
 
 @main
@@ -33,24 +37,69 @@ struct RenderMermaid: AsyncParsableCommand {
 	)
 	var verbose: Bool = false
 
+	@Option(
+		name: [.customShort("o"), .customLong("outdir")],
+		help: "Output directory to create diagrams in."
+	)
+	var outputPath: String?
+
+	func log(_ string: @autoclosure () -> String) {
+		guard verbose else { return }
+		try! FileHandle.standardOutput.write(string().data())
+	}
+
 	func markdownDocument() throws -> Markdown.Document {
 		if let inputFile {
 			return try Document(parsing: inputFile)
 		} else if let string = readLine() {
 			return Document(parsing: string)
 		} else {
-			throw ArgumentParser.ValidationError("Provide either STDIN or \(_inputFile.description)")
+			throw ArgumentParser.ValidationError(
+				"Provide either STDIN or \(_inputFile.description)")
 		}
+	}
+
+	func outputDirectory() throws -> URL {
+		if let outputPath {
+			return try directory(forPath: outputPath)
+		} else {
+			return try temporaryOutputDirectory()
+		}
+	}
+
+	private func temporaryOutputDirectory() throws -> URL {
+		let tmpDir = FileManager.default.temporaryDirectory
+
+		log("Using temporary directory: “\(tmpDir)”")
+
+		do {
+			try FileManager.default.createDirectory(
+				at: tmpDir, withIntermediateDirectories: true)
+			return tmpDir
+		} catch let error {
+			throw RenderError(
+				message: "Could not create temporary directory at “\(tmpDir)”: \(error)")
+		}
+	}
+
+	private func directory(forPath path: String) throws -> URL {
+		var isDirectory: ObjCBool = false
+		let pathExists = FileManager.default.fileExists(
+			atPath: path, isDirectory: &isDirectory)
+		guard pathExists else {
+			throw RenderError(message: "Output directory at “\(path)” does not exist")
+		}
+		guard isDirectory.boolValue == true else {
+			throw RenderError(message: "Output path at “\(path)” is not a directory")
+		}
+		return URL(fileURLWithPath: path, isDirectory: true)
 	}
 
 	mutating func run() async throws {
 		let document = try markdownDocument()
-
-		let tmpDir = FileManager.default.temporaryDirectory
-		try FileManager.default.createDirectory(at: tmpDir, withIntermediateDirectories: true)
 		let mermaidPath = self.mermaidPath
 		let renderer = MarkdownBlockRenderer<Markdown.CodeBlock, String>(
-			outputDirectory: tmpDir,
+			outputDirectory: try outputDirectory(),
 			fileExtension: "png",
 			extractContent: \.code
 		) { [verbose] (code: String, url: URL) in
