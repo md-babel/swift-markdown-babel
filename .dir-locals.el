fa370f34a369a3c8dd32d26cdeb689195e0c5a2b
Christian Tietze <me@christiantietze.de>
2025-04-04 17:42:24 +0200

    add .dir-locals.el to match swift format config

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((prog-mode . ((fill-column . 100)
+			   (eval . (indent-tabs-mode 1)))))
