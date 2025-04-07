d156b5efec00b0941c16a427b493364c21ef5308
Christian Tietze <me@christiantietze.de>
2025-04-07 06:45:54 +0200

    auto-format buffers with eglot (according to swift-format rules)

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((prog-mode . ((fill-column . 100)
-			   (eval . (indent-tabs-mode 1)))))
+			   (eval . (progn (indent-tabs-mode 1)
+							  (add-hook 'before-save-hook
+										(lambda () (when eglot--managed-mode (eglot-format-buffer)))
+										nil t))))))
