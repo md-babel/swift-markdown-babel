5e2a2ebbc4a5f523de33eced93a326e23b3ac462
Christian Tietze <me@christiantietze.de>
2025-04-11 16:19:21 +0200

    strip trailing newslines and separator between metadata & result blocks

--- Sources/MarkdownBabel/Execute/Response/ExecuteResponse+rendering.swift
+++ Sources/MarkdownBabel/Execute/Response/ExecuteResponse+rendering.swift
@@ -1,34 +1,38 @@
 import Markdown
 
 extension ExecuteResponse.ExecutionResult {
-	func outputBlocks(reusing oldResult: ExecutableContext.Result?) -> [any BlockMarkup] {
-		guard let output else { return [] }
+	fileprivate func renderedOutputBlocks(reusing oldResult: ExecutableContext.Result?) -> String? {
+		guard let output else { return nil }
 		let header: String = oldResult?.header ?? "Result:"
 		return [
-			HTMLCommentBlock(htmlBlock: HTMLBlock("<!--\(header)-->"))!,
-			CodeBlock(language: nil, output),
-		]
+			HTMLCommentBlock(htmlBlock: HTMLBlock("<!--\(header)-->"))!.format(),
+			CodeBlock(language: nil, output.trimmingCharacters(in: .newlines)).format(
+				options: .init(useCodeFence: .always)
+			),
+		].joined(separator: "\n")
 	}
 
-	func errorBlocks(reusing oldError: ExecutableContext.Error?) -> [any BlockMarkup] {
-		guard let message = self.error else { return [] }
+	fileprivate func renderedErrorBlocks(reusing oldError: ExecutableContext.Error?) -> String? {
+		guard let message = self.error else { return nil }
 		let header: String = oldError?.header ?? "Error:"
 		return [
-			HTMLCommentBlock(htmlBlock: HTMLBlock("<!--\(header)-->"))!,
-			CodeBlock(language: nil, message),
-		]
+			HTMLCommentBlock(htmlBlock: HTMLBlock("<!--\(header)-->"))!.format(),
+			CodeBlock(language: nil, message.trimmingCharacters(in: .newlines)).format(
+				options: .init(useCodeFence: .always)
+			),
+		].joined(separator: "\n")
 	}
 }
 
 extension ExecuteResponse {
 	public func rendered() -> String {
-		let document = Markdown.Document(
-			[
-				[self.executableContext.codeBlock],
-				executionResult.outputBlocks(reusing: executableContext.result),
-				executionResult.errorBlocks(reusing: executableContext.error),
-			].flatMap { $0 }
-		)
-		return document.format(options: .init(useCodeFence: .always))
+		return [
+			// CodeBlock.format() on its own will prepend two empty newlines; a Document with just a CodeBlock won't, so we wrap it:
+			Markdown.Document(self.executableContext.codeBlock).format(options: .init(useCodeFence: .always)),
+			executionResult.renderedOutputBlocks(reusing: executableContext.result),
+			executionResult.renderedErrorBlocks(reusing: executableContext.error),
+		]
+		.compactMap { $0 }
+		.joined(separator: "\n\n")
 	}
 }
