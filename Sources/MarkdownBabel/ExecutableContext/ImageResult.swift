d6a01140493f4068b71bea75ea6490a81034f25e
Christian Tietze <me@christiantietze.de>
2025-04-21 08:39:38 +0200

    expose existing ImageResult hash, stored in alt text

--- Sources/MarkdownBabel/ExecutableContext/ImageResult.swift
+++ Sources/MarkdownBabel/ExecutableContext/ImageResult.swift
@@ -6,12 +6,15 @@ public struct ImageResult: ResultMarkup {
 
 	public let title: String
 	public let source: String
+	public let alt: String
+	public var contentHash: String { alt }
 	public var content: String { source }
 	public let range: SourceRange
 
 	public init(_ base: DocumentEmbedded<ImageParagraph>) {
 		self.title = base.markup.image.title ?? ""
 		self.source = base.markup.image.source ?? ""
+		self.alt = base.markup.alt
 		self._debugDescription = { base.markup.debugDescription(options: $0) }
 		self._nextSibling = { base.markup.nextSibling() }
 		self.range = base.range
